4dc605812bc0c3d3bd4dc6b21f9095d4935ffca7
thomas <bezille.thomasb@gmail.com>
2024-12-20 16:11:19 +0100

    📦 NEW : Create models - Create Task model - Create Category model - Make migrations - Migrate migrations

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+    slug = models.SlugField()
+
+class Task(models.Model):
+    description = models.CharField(max_length=300)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
